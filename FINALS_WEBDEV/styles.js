25515169cfd60ed9a0b5faa52f1e59f65a1d0fd6
beedollyuh <152391739+beedollyuh@users.noreply.github.com>
2024-07-16 11:18:17 +0800

    Add files via upload

---
+++ FINALS_WEBDEV/styles.js
@@ -0,0 +1,29 @@
+var acc = document.getElementsByClassName("accord");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+.accordion:after {
+    content: '\02795'; /* Unicode character for "plus" sign (+) */
+    font-size: 13px;
+    color: #777;
+    float: right;
+    margin-left: 5px;
+  }
+  
+  .active:after {
+    content: "\2796"; /* Unicode character for "minus" sign (-) */
+  }
